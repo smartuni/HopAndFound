d6590b02116d9bfe5ef685035d633af44ded87be
Dennis Kirsch <dennis.kirsch@haw-hamburg.de>
2016-10-17 13:36:53 +0200

    fix error in main

--- src/main.c
+++ src/main.c
@@ -4,27 +4,28 @@
 #include "msg.h"
 #include "shell.h"
 #include "udp.h"
+#include "xtimer.h"
 
 int main(void)
 {
 	int sn;
 	TPacket p;
 	
-    if (udp_server_start() != 0)
-    {
-		printf("Failed to start udp server\n");
-    }
-    
-    p.header = 1;
-    p.payload = 42;
-    
-    sn = 0;
-    while(1)
-    {
+	if (udp_server_start() != 0)
+	{
+		puts("Failed to start udp server\n");
+	}
+	
+	p.header = 1;
+	p.payload = 42;
+	
+	sn = 0;
+	while(1)
+	{
 		p.sequence_number = sn;
 		udp_send(&p);
 		xtimer_sleep(1);
-    }
+	}
 
-    return 0;
+	return 0;
 }
