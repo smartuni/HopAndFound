74f7196d7df0f18af682607aae19048b326dfdcb
HaukeSondermann <hauke.sondermann@gmail.com>
2016-10-24 17:32:48 +0200

    added packaging / changed char/int to uintx_t

--- src/HAF_protocol.h
+++ src/HAF_protocol.h
@@ -1,39 +1,35 @@
-
 #ifndef HAF_PROTOCOL_H
 #define HAF_PROTOCOL_H
 
-#define MAX_NODES 6
+#include <stdint.h>
+
+#define MAX_NODES (6)
 
 typedef enum pkg_type{
 	HEARTBEAT,
 	LOCALIZATION_REQUEST,
 	LOCALIZATION_REPLY,
 	CALL_FOR_HELP
-}pkg_type_t;
+} pkg_type_t;
 
+typedef struct __attribute__((packed)) {
+	uint8_t type;
+} heartbeat_t;
 
-typedef struct heartbeat{
-	char type;
-}heartbeat_t;
+typedef struct __attribute__((packed)) {
+	uint8_t type;
+} localization_request_t;
 
+typedef struct __attribute__((packed)) {
+	uint8_t type;
+	uint8_t node_id;
+} localization_reply_t;
 
-typedef struct localization_request{
-	char type;
-}localization_request_t;
+typedef struct __attribute__((packed)) {
+	uint8_t type;
+	uint32_t seq_nr;
+	uint8_t mi_id;
+	uint8_t[MAX_NODES] node_list;
+} call_for_help_t;
 
-
-typedef struct localization_reply{
-	char type;
-	char node_id;
-}localization_reply_t;
-
-
-typedef struct call_for_help{
-	char type;
-	int seq_nr;
-	char mi_id;
-	char[MAX_NODES] node_list;
-}call_for_help_t;
-
-
-#endif //HAF_PROTOCOL_H
+#endif /* HAF_PROTOCOL_H */
