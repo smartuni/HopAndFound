74e9d84c0304c0d133d8e09d89136f0d795ae448
Dennis Kirsch <dennis.kirsch@haw-hamburg.de>
2016-10-24 11:16:41 +0000

    define node id

--- src/localization_request.c
+++ src/localization_request.c
@@ -1,9 +1,12 @@
 #include "localization.h"
+#include "connection.h"
+
+#define NODE_ID 1 //unique id to identify nodes
 
 void handle_localization_request(localization_request_t* p){
 	localization_reply_t ret_pkg;
 	ret_pkg.type = LOCALIZATION_REPLY;
-	ret_pkg.node_id = 1;//TODO
+	ret_pkg.node_id = NODE_ID;
 	udp_send(&ret_pkg, sizeof(ret_pkg));
 }
 
