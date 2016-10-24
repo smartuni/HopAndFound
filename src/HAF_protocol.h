706e0b0f6a7de80d461c3bfc767d66934cdc1fce
thorben <thorben.schnirpel@haw-hamburg.de>
2016-10-24 12:09:36 +0200

    Added HAF_protocol -header

---
+++ src/HAF_protocol.h
@@ -0,0 +1,39 @@
+
+#ifndef HAF_PROTOCOL_H
+#define HAF_PROTOCOL_H
+
+#define MAX_NODES 6
+
+typedef enum pkg_type{
+	HEARTBEAT,
+	LOCALIZATION_REQUEST,
+	LOCALIZATION_REPLY,
+	CALL_FOR_HELP
+}pkg_type_t;
+
+
+typedef struct heartbeat{
+	char type;
+}heartbeat_t;
+
+
+typedef struct localization_request{
+	char type;
+}localization_request_t;
+
+
+typedef struct localization_reply{
+	char type;
+	char node_id;
+}localization_reply_t;
+
+
+typedef struct call_for_help{
+	char type;
+	int seq_nr;
+	char mi_id;
+	char[MAX_NODES] node_list;
+}call_for_help_t;
+
+
+#endif //HAF_PROTOCOL_H
