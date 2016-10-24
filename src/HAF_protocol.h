b9058434acb96b849f71911e8539d4faad24315c
HaukeSondermann <hauke.sondermann@gmail.com>
2016-10-24 20:56:41 +0200

    merge correction

--- src/HAF_protocol.h
+++ src/HAF_protocol.h
@@ -20,25 +20,16 @@ typedef struct __attribute__((packed)) {
 	uint8_t type;
 } localization_request_t;
 
-<<<<<<< HEAD
 typedef struct __attribute__((packed)) {
 	uint8_t type;
 	uint8_t node_id;
 } localization_reply_t;
-=======
+
 typedef struct call_for_help{
 	uint8_t type;
 	uint32_t seq_nr;
 	uint8_t mi_id;
 	uint8_t node_list[MAX_NODES];
 }call_for_help_t;
->>>>>>> c9f89c45c52eb64f22b7140dbf6b1f60e1e30a75
-
-typedef struct __attribute__((packed)) {
-	uint8_t type;
-	uint32_t seq_nr;
-	uint8_t mi_id;
-	uint8_t[MAX_NODES] node_list;
-} call_for_help_t;
 
 #endif /* HAF_PROTOCOL_H */
